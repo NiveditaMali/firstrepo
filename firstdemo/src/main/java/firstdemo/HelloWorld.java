dc768c843c25d854dca36b1b8f059e94d1de1c60
nikrishn <nikrishn@DIN60016421.corp.capgemini.com>
2021-08-17 11:06:42 +0530

    commit2 conflict

--- firstdemo/src/main/java/firstdemo/HelloWorld.java
+++ firstdemo/src/main/java/firstdemo/HelloWorld.java
@@ -3,9 +3,10 @@ package firstdemo;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello Nivedita here");
+		
 		for(int i=1;i<5;i++) {
 			System.out.println(i);
+			System.out.println("\n");
 		}
 	}
 
